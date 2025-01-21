ff2478937a5926ee9ce959b50f5db14e01053b95
ManuGorantla <163234385+ManuGorantla@users.noreply.github.com>
2025-01-20 16:07:14 -0800

    Add files via upload

--- RPS.py
+++ RPS.py
@@ -1,31 +1,38 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. 
+# It is not a very good player so you will need to change the code to pass the challenge.
+import random
 
 def player(prev_play, opponent_history=[]):
     if prev_play:
         opponent_history.append(prev_play)
 
-    play_order = {}
-    n = 4  
+    plays = {}
+    n = 4
 
     if len(opponent_history) > n:
         for i in range(len(opponent_history) - n):
-            seq = ''.join(opponent_history[i:i+n])
-            next_move = opponent_history[i+n]
-            if seq not in play_order:
-                play_order[seq] = {'R': 0, 'P': 0, 'S': 0}
-            play_order[seq][next_move] += 1
+            second = opponent_history[i + n]
+            moves = ''.join(opponent_history[i:(i + n)])
 
-        recent_sequence = ''.join(opponent_history[-n:])
+            if moves not in plays:
+                plays[moves] = {'R': 0, 'P': 0, 'S': 0}
+
+            plays[moves][second] += 1
+
+        last = ''.join(opponent_history[-n:])
         
-        if recent_sequence in play_order:
-            prediction = max(play_order[recent_sequence], key=play_order[recent_sequence].get)
+        if last in plays:
+            prediction = max(plays[last], key=plays[last].get)
         else:
             move_counts = {'R': 0, 'P': 0, 'S': 0}
+
             for move in opponent_history:
                 move_counts[move] += 1
+
             prediction = max(move_counts, key=move_counts.get)
     else:
-        prediction = 'R'
+        prediction = random.choice(['R', 'P', 'S'])
 
-    counter_moves = {'R': 'P', 'P': 'S', 'S': 'R'}
-    return counter_moves[prediction]
+    reverse = {'R': 'P', 'P': 'S', 'S': 'R'}
+
+    return reverse[prediction]
